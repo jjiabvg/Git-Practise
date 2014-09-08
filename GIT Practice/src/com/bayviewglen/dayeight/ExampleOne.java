1613d380bd3b7ef160cabc40952fb446fd2177ef
jjia <jjia@bayviewglen.ca>
2014-09-08 13:53:44 -0400

    Merge branch 'master' of https://github.com/jjiabvg/Git-Practise
    Conflicts:
    	GIT Practice/src/com/bayviewglen/dayeight/ExampleOne.java

--- GIT Practice/src/com/bayviewglen/dayeight/ExampleOne.java
+++ GIT Practice/src/com/bayviewglen/dayeight/ExampleOne.java
@@ -3,14 +3,16 @@ package com.bayviewglen.dayeight;
 public class ExampleOne {
 
 	public static void main(String[] args) {
-
+<<<<<<< HEAD
 		final int POSITIVE_INTEGER = 6;
 		final int NEGATIVE_INTEGER = -9;
-
-	
+=======
+		final int POSITIVE_INTEGER = 7;
+		final int NEGATIVE_INTEGER = -8;
+>>>>>>> origin/master
 		final int ZERO = 0;
 		
-		final double POSITIVE_DOUBLE = 5.5;
+		final double POSITIVE_DOUBLE = 5.0;
 		final double NEGATIVE_DOUBLE = -8.0;
 		
 	//	System.out.println(POSITIVE_INTEGER/ZERO);
